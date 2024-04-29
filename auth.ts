caf16dca2a9a2b02f4437d6cd04b32f41722e99a
Gabriela França <gabifmouraroc@outlook.com>
2024-04-29 15:05:15 -0300

    login com credenciais com next auth

---
+++ auth.ts
@@ -0,0 +1,47 @@
+import NextAuth from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+import db from "@/lib/db"
+import { compareSync } from 'bcrypt-ts';
+
+// const {handlers, auth}= NextAuth({providers: [Credentials]})
+
+export const {
+  handlers: { GET, POST },
+  auth,
+} = NextAuth({
+  providers: [Credentials({
+    credentials: {
+      email: {
+
+      },
+      password: {}
+    },
+    async authorize(credentials) {
+      const email = credentials.email as string
+      const password = credentials.password as string
+
+      if(!credentials.email || !credentials.password) {
+        return null
+      }
+    
+      const user = await db.user.findUnique({
+        where: {
+          email: email
+        }
+      })
+
+      if (!user) {
+        return null
+      }
+
+      const matches = compareSync(password, user.password ?? '')
+
+      if(matches) {
+        return { id: user.id, name: user.name, email: user.email}
+      } 
+      
+      return null
+      
+    }
+  })],
+});
